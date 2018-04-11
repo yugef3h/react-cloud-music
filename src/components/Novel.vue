af3f5baa952a8e2f47fa9b6b23812fded99a6286
yuqlpt <yuqinglong@ptit365.com>
2018-04-11 16:55:47 +0800

    novel v2

--- src/components/Novel.vue
+++ src/components/Novel.vue
@@ -4,6 +4,7 @@
     <p>2.先从数据库搜索，数据库没有再去爬取，并保存于数据库。注意: 搜索小说若为第一次不能频繁请求。</p>
     <input type="text" placeholder="请输入书名或作者搜索" v-model="keyn">
     <button type="button" @click="search">搜索小说</button>
+    <p><a href="javascript:" @click="search" v-show="show">{{tips}}</a></p>
     <ul>
       <li v-for="(item,index) in menus" :key="index">
         <a :href="item.split('+')[1]">{{item.split('+')[0]}}</a>
@@ -17,7 +18,9 @@
     data () {
       return {
         keyn: '',
-        menus: []
+        menus: [],
+        tips: '',
+        show: false
       }
     },
     methods: {
@@ -27,8 +30,16 @@
           this.$reqs.post('/users/novel', {
             keyn: this.keyn.trim()
           }).then(res => {
-            //console.log(res.data)
-            _this.menus = res.data.content.split('-');
+            console.log(res)
+            if (res.data.tip) {
+              this.show = true;
+              _this.tips = res.data.tip;
+            } else {
+              this.show = false;
+              _this.menus = res.data[0].titles.split('-');
+            }
+
+
         }).catch(err => {
             console.log(err)
         })
